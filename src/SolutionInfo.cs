30d79942cbd744165e13ac18d1449037b2d791c8
pelle henriksson <pelle.henriksson@gmail.com>
2011-04-19 14:56:48 +0200

    added solutioninfo.cs

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Crepido Systems AB")]
+[assembly: AssemblyProduct("ElmahOfflineViewer")]
+[assembly: AssemblyCopyright("Copyright © Crepido Systems AB 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

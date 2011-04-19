1d62fd01a00ec770096392950feeab78c3d3f5b6
pelle henriksson <pelle.henriksson@gmail.com>
2011-04-19 22:01:35 +0200

    added dateinterval

---
+++ src/Crepido.ElmahOfflineViewer.Core/Common/DateInterval.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Crepido.ElmahOfflineViewer.Core.Common
+{
+	public class DateInterval
+	{
+		public DateInterval(DateTime startDate, DateTime endDate)
+		{
+			StartDate = startDate;
+			EndDate = endDate;
+		}
+
+		public DateTime StartDate { get; private set; }
+
+		public DateTime EndDate { get; private set; }
+	}
+}
---
+++ src/Crepido.ElmahOfflineViewer.UnitTests/Common/DateIntervalTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Crepido.ElmahOfflineViewer.Core.Common;
+using NUnit.Framework;
+
+namespace Crepido.ElmahOfflineViewer.UnitTests.Common
+{
+	[TestFixture]
+	public class DateIntervalTests : UnitTestBase
+	{
+		[Test]
+		public void Ctor_SetStartAndEndDate()
+		{
+			// arrange
+			var startDate = DateTime.Today;
+			var endDate = DateTime.Today.AddDays(1);
+
+			// act
+			var interval = new DateInterval(startDate, endDate);
+
+			// assert
+			Assert.That(interval.StartDate, Is.EqualTo(startDate));
+			Assert.That(interval.EndDate, Is.EqualTo(endDate));
+		}
+	}
+}

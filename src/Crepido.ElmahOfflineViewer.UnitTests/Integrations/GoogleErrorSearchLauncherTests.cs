daf50f729d9aaca27ba74fba9dd6b58cab94b521
pelle henriksson <pelle.henriksson@gmail.com>
2011-05-08 23:17:13 +0200

    google web serach tests

---
+++ src/Crepido.ElmahOfflineViewer.UnitTests/Integrations/GoogleErrorSearchLauncherTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Crepido.ElmahOfflineViewer.Core.Integrations;
+using NUnit.Framework;
+
+namespace Crepido.ElmahOfflineViewer.UnitTests.Integrations
+{
+	[TestFixture]
+	public class GoogleErrorSearchLauncherTests : UnitTestBase
+	{
+		[Test]
+		public void Ctor_ErrorLogIsNull_Throws()
+		{
+			// act
+			var result = Assert.Throws<ArgumentNullException>(() => new GoogleErrorSearchLauncher(null));
+
+			// assert
+			Assert.That(result, Is.Not.Null);
+			Assert.That(result.ParamName, Is.EqualTo("errorLog"));
+		}
+
+		[Test]
+		public void Ctor_SetsErrorLog()
+		{
+			// arrange
+			var errorLog = CreateFakeErrorLog();
+
+			// act
+			var launcher = new GoogleErrorSearchLauncher(errorLog);
+
+			// assert
+			Assert.That(launcher.ErrorLog, Is.EqualTo(errorLog));
+		}
+
+		[Test]
+		public void Ctor_BuildUrlWithTypeAndSourceAsParameters()
+		{
+			// arrange
+			var errorLog = CreateFakeErrorLog();
+			var launcher = new GoogleErrorSearchLauncher(errorLog);
+
+			// act
+			var result = launcher.BuildSearchUrl();
+
+			// assert
+			Assert.That(result, Is.EqualTo("http://www.google.com/search?q=System.InvalidOperationException+System.Web.Mvc"));
+		}
+	}
+}

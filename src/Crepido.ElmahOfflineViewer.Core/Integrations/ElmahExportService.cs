2bf3583728f30838e8e347157c937b92167e1429
pelle henriksson <pelle.henriksson@gmail.com>
2011-05-17 08:15:11 +0200

    starting work on integration elmahexp.exe

---
+++ src/Crepido.ElmahOfflineViewer.Core/Integrations/ElmahExportService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Crepido.ElmahOfflineViewer.Core.Infrastructure;
+using Crepido.ElmahOfflineViewer.Core.Infrastructure.Logging;
+using Crepido.ElmahOfflineViewer.Core.Infrastructure.Settings;
+
+namespace Crepido.ElmahOfflineViewer.Core.Integrations
+{
+	public class ElmahExportService
+	{
+		private ISettingsManager _settingsManager;
+		private readonly ILog _log;
+		private readonly IProcessHelper _processHelper;
+
+		public ElmahExportService(ISettingsManager settingsManager, ILog log, IProcessHelper processHelper)
+		{
+			_settingsManager = settingsManager;
+			_log = log;
+			_processHelper = processHelper;
+		}
+		
+		public string Download(string url)
+		{
+			// path to the schema exp
+			var exePath = Path.Combine(Directory.GetCurrentDirectory(), "elmahexp.exe");
+
+			// build download directory from path
+			var downloadDirectory = Path.Combine(Directory.GetCurrentDirectory(), "www.someapp.com");
+
+			// call expoter with url number of logs
+
+			// return download directory
+			
+			// handle all errors from elmahexp.exe)
+
+			try
+			{
+				_processHelper.Run("");
+
+				return string.Empty;
+			}
+			catch(Exception ex)
+			{
+				_log.Error(ex);
+				throw;
+			}
+		}
+	}
+}
---
+++ src/Crepido.ElmahOfflineViewer.UnitTests/Integrations/ElmahExportServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Crepido.ElmahOfflineViewer.Core.Integrations;
+using Crepido.ElmahOfflineViewer.TestHelpers.Fakes;
+using NUnit.Framework;
+
+namespace Crepido.ElmahOfflineViewer.UnitTests.Integrations
+{
+	[TestFixture]
+	public class ElmahExportServiceTests : UnitTestBase
+	{
+		[Test]
+		public void FirstTest()
+		{
+			// arrange
+			var service = new ElmahExportService(new FakeSettingsManager(), new FakeLog(), new FakeProcessHelper());
+			const string url = "http://www.someapp.com/elmah.axd";
+			
+			// act
+			var result = service.Download(url);
+
+			// assert
+			Assert.That(result, Is.EqualTo(@"c:\test\www.someapp.com"));
+		}
+	}
+}

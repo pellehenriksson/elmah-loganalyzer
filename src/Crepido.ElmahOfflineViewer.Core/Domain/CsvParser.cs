75554f30a4aaeaba3963a39ca929585d40c84659
azizatif <azizatif@localhost>
2011-05-19 01:14:41 +0200

    Simplified skipping of CSV headers

--- src/Crepido.ElmahOfflineViewer.Core/Domain/CsvParser.cs
+++ src/Crepido.ElmahOfflineViewer.Core/Domain/CsvParser.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Microsoft.VisualBasic.FileIO;
 
 namespace Crepido.ElmahOfflineViewer.Core.Domain
 {
+	using Crepido.ElmahOfflineViewer.Core.Common;
+
 	public class CsvParser
 	{
 		public IEnumerable<KeyValuePair<Uri, DateTime>> Parse(string content)
@@ -15,21 +18,11 @@ namespace Crepido.ElmahOfflineViewer.Core.Domain
 			using (var parser = new TextFieldParser(stream, Encoding.Unicode) { TextFieldType = FieldType.Delimited })
 			{
 				parser.SetDelimiters(",");
-
-				var hasPassedHeaderRow = false;
-				while (!parser.EndOfData)
+				foreach (var currentRow in parser.Read().Skip(/* headers */ 1))
 				{
-					var currentRow = parser.ReadFields();
-
-					if (hasPassedHeaderRow)
-					{
-						var date = Convert.ToDateTime(currentRow[2]);
-						var detailsUrl = new Uri(currentRow[9]);
-
-						yield return new KeyValuePair<Uri, DateTime>(detailsUrl, date);
-					}
-
-					hasPassedHeaderRow = true;
+					var date = Convert.ToDateTime(currentRow[2]);
+					var detailsUrl = new Uri(currentRow[9]);
+					yield return new KeyValuePair<Uri, DateTime>(detailsUrl, date);
 				}
 			}
 		}

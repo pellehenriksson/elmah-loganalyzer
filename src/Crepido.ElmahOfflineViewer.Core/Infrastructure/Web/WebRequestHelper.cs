42b08587a06caf89373a8fef0d1b3abef777b0cf
azizatif <azizatif@localhost>
2011-05-19 08:57:52 +0200

    SA1515 fix in WebRequestHelper

--- src/Crepido.ElmahOfflineViewer.Core/Infrastructure/Web/WebRequestHelper.cs
+++ src/Crepido.ElmahOfflineViewer.Core/Infrastructure/Web/WebRequestHelper.cs
@@ -13,6 +13,7 @@ namespace Crepido.ElmahOfflineViewer.Core.Infrastructure.Web
 			using (var client = new WebClient())
 			{
 				var response = client.OpenRead(url);
+				
 				// TODO Check content-type is the expected one!
 				// TODO Use charset in response Content-Type header
 				using (var reader = new StreamReader(response))

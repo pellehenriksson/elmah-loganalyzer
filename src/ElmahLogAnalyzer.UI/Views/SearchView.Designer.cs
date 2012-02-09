daa0bf518ec577d2620ad48b619f6eb8bb5946c9
per henriksson <per.henriksson@crepido.com>
2012-02-09 11:00:13 +0100

    refactoring

--- src/ElmahLogAnalyzer.UI/Views/SearchView.Designer.cs
+++ src/ElmahLogAnalyzer.UI/Views/SearchView.Designer.cs
@@ -31,55 +31,38 @@ namespace ElmahLogAnalyzer.UI.Views
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this._horizontalSplitContainer = new System.Windows.Forms.SplitContainer();
 			this._filterView = new ElmahLogAnalyzer.UI.Views.Partials.SearchFilterView();
+			this._verticalSplitContainer = new System.Windows.Forms.SplitContainer();
 			this._resultView = new ElmahLogAnalyzer.UI.Views.Partials.SearchResultView();
 			this._detailsView = new ElmahLogAnalyzer.UI.Views.Partials.SearchDetailsView();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.Panel2.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-			this.splitContainer2.Panel1.SuspendLayout();
-			this.splitContainer2.Panel2.SuspendLayout();
-			this.splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this._horizontalSplitContainer)).BeginInit();
+			this._horizontalSplitContainer.Panel1.SuspendLayout();
+			this._horizontalSplitContainer.Panel2.SuspendLayout();
+			this._horizontalSplitContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this._verticalSplitContainer)).BeginInit();
+			this._verticalSplitContainer.Panel1.SuspendLayout();
+			this._verticalSplitContainer.Panel2.SuspendLayout();
+			this._verticalSplitContainer.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// splitContainer1
+			// _horizontalSplitContainer
 			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this._horizontalSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._horizontalSplitContainer.Location = new System.Drawing.Point(0, 0);
+			this._horizontalSplitContainer.Name = "_horizontalSplitContainer";
+			this._horizontalSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
 			// 
-			// splitContainer1.Panel1
+			// _horizontalSplitContainer.Panel1
 			// 
-			this.splitContainer1.Panel1.Controls.Add(this._filterView);
+			this._horizontalSplitContainer.Panel1.Controls.Add(this._filterView);
 			// 
-			// splitContainer1.Panel2
+			// _horizontalSplitContainer.Panel2
 			// 
-			this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-			this.splitContainer1.Size = new System.Drawing.Size(1279, 607);
-			this.splitContainer1.SplitterDistance = 211;
-			this.splitContainer1.TabIndex = 0;
-			// 
-			// splitContainer2
-			// 
-			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer2.Name = "splitContainer2";
-			// 
-			// splitContainer2.Panel1
-			// 
-			this.splitContainer2.Panel1.Controls.Add(this._resultView);
-			// 
-			// splitContainer2.Panel2
-			// 
-			this.splitContainer2.Panel2.Controls.Add(this._detailsView);
-			this.splitContainer2.Size = new System.Drawing.Size(1279, 392);
-			this.splitContainer2.SplitterDistance = 271;
-			this.splitContainer2.TabIndex = 0;
+			this._horizontalSplitContainer.Panel2.Controls.Add(this._verticalSplitContainer);
+			this._horizontalSplitContainer.Size = new System.Drawing.Size(1279, 607);
+			this._horizontalSplitContainer.SplitterDistance = 211;
+			this._horizontalSplitContainer.TabIndex = 0;
 			// 
 			// _filterView
 			// 
@@ -88,6 +71,23 @@ namespace ElmahLogAnalyzer.UI.Views
 			this._filterView.Name = "_filterView";
 			this._filterView.Size = new System.Drawing.Size(1279, 211);
 			this._filterView.TabIndex = 0;
+			// 
+			// _verticalSplitContainer
+			// 
+			this._verticalSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._verticalSplitContainer.Location = new System.Drawing.Point(0, 0);
+			this._verticalSplitContainer.Name = "_verticalSplitContainer";
+			// 
+			// _verticalSplitContainer.Panel1
+			// 
+			this._verticalSplitContainer.Panel1.Controls.Add(this._resultView);
+			// 
+			// _verticalSplitContainer.Panel2
+			// 
+			this._verticalSplitContainer.Panel2.Controls.Add(this._detailsView);
+			this._verticalSplitContainer.Size = new System.Drawing.Size(1279, 392);
+			this._verticalSplitContainer.SplitterDistance = 271;
+			this._verticalSplitContainer.TabIndex = 0;
 			// 
 			// _resultView
 			// 
@@ -109,26 +109,26 @@ namespace ElmahLogAnalyzer.UI.Views
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this._horizontalSplitContainer);
 			this.Name = "SearchView";
 			this.Size = new System.Drawing.Size(1279, 607);
 			this.Load += new System.EventHandler(this.SearchViewLoad);
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			this.splitContainer1.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
-			this.splitContainer2.Panel1.ResumeLayout(false);
-			this.splitContainer2.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-			this.splitContainer2.ResumeLayout(false);
+			this._horizontalSplitContainer.Panel1.ResumeLayout(false);
+			this._horizontalSplitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this._horizontalSplitContainer)).EndInit();
+			this._horizontalSplitContainer.ResumeLayout(false);
+			this._verticalSplitContainer.Panel1.ResumeLayout(false);
+			this._verticalSplitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this._verticalSplitContainer)).EndInit();
+			this._verticalSplitContainer.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.SplitContainer _horizontalSplitContainer;
+		private System.Windows.Forms.SplitContainer _verticalSplitContainer;
 		private SearchFilterView _filterView;
 		private SearchResultView _resultView;
 		private SearchDetailsView _detailsView;

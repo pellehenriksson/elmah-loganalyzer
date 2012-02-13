f83affaae0b601a9bd5524a82387285d2477ba8a
Pelle Henriksson <pelle.henriksson@gmail.com>
2012-02-13 12:41:32 +0100

    state management for error log sources

---
+++ src/ElmahLogAnalyzer.Core/Infrastructure/Dependencies/DataSourceScopeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ElmahLogAnalyzer.Core.Common;
+
+namespace ElmahLogAnalyzer.Core.Infrastructure.Dependencies
+{
+	public static class DataSourceScopeController
+	{
+		private static object _keepAlive = new object();
+
+		public static string Source { get; private set; }
+		
+		public static string Connection { get; private set; }
+
+		public static object KeepAlive
+		{
+			get { return _keepAlive; }
+		}
+
+		public static void SetNewSource(string source, string connection)
+		{
+			if (!source.HasValue())
+			{
+				throw new InvalidOperationException("The source is undefined");
+			}
+
+			Source = source;
+			Connection = connection;
+
+			_keepAlive = new object();
+		}
+	}
+}
